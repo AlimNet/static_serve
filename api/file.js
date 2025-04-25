f011b9d00fc970edea0d7005c178f7c7f3d186e7
AlimNet <a.abibullaev2019@gmail.com>
2025-04-25 21:51:21 +0300

    Update file.js

--- api/file.js
+++ api/file.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   // Устанавливаем заголовки CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');  // Разрешаем Authorization
   
   // Устанавливаем правильный тип контента
   res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
